12fcc46864659c2608757369948d3262dd871c5e
lucy <lyog@qq.com>
2019-01-06 20:22:17 +0800

    Merge branch 'marry'
    # Conflicts:
    #	poi/src/main/java/poi/Test.java

--- poi/src/main/java/poi/Test.java
+++ poi/src/main/java/poi/Test.java
@@ -6,6 +6,7 @@ public class Test {
     public static void main(String[] args) {
         //edit by lucy
         //edit by marry1
+        //lucy branch
         List list = ExcelReadUtil.<User>readExcel("h:/user.xlsx","poi.User");
         System.out.print(list);
     }
