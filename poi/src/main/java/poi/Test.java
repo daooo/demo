c1a17329bd250ff46ebb3c024a8257be44e55109
lucy <lyog@qq.com>
2019-01-06 18:30:03 +0800

    lucy commit

--- poi/src/main/java/poi/Test.java
+++ poi/src/main/java/poi/Test.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-
         List list = ExcelReadUtil.<User>readExcel("h:/user.xlsx","poi.User");
         System.out.print(list);
     }
